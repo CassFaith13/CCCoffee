97d37c7148ebccb0c5afb0f6da5b2059cf8f94e0
Celio Arias <ariascelio@gmail.com>
2023-01-04 20:26:50 -0500

    menuItemEntity

--- CCCoffee.Data/Entities/MenuItemEntity.cs
+++ CCCoffee.Data/Entities/MenuItemEntity.cs
@@ -2,6 +2,14 @@ namespace CCCoffee.Data.Entities
 {
     public class MenuItemEntity
     {
-        
-    }
+     public int MenuItemId { get; set; }
+
+    public string MealName { get; set; } = null!;
+
+    public string MealDescription { get; set; } = null!;
+
+    public decimal MealPrice { get; set; }
+
+    private string OrderId {get; set;}
+    } 
 }
