8ea8631eafe528eb752c15985bb927cda5b2adcc
CassFaith13 <cassandra.faith@icloud.com>
2023-01-04 19:04:44 -0500

    customer order entity

--- CCCoffee.Data/Entities/CustomerOrderEntity.cs
+++ CCCoffee.Data/Entities/CustomerOrderEntity.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CCCoffee.Data.Entities
 {
     public class CustomerOrderEntity
     {
-        
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public int MenuItemId { get; set; }
+        public virtual CustomerEntity Customer { get; set; }
+        public virtual MenuItemEntity MenuItem { get; set; }
     }
 }
