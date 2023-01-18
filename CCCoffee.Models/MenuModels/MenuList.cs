5addd6f0538b287d6646442bd28780ce55a28f67
Cassandra Faith <105570288+CassFaith13@users.noreply.github.com>
2023-01-18 18:46:41 -0500

    Update MenuList.cs

--- CCCoffee.Models/MenuModels/MenuList.cs
+++ CCCoffee.Models/MenuModels/MenuList.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CCCoffee.Models.MenuModels
 {
     public class MenuList
     {
-        public int MenuItemId { get; set;}
-        public string? MealName { get; set;}
-        public decimal MealPrice { get; set;}
+        public int MenuItemId { get; set; }
+        public string? MealName { get; set; }
+        public decimal MealPrice { get; set; }
     }
 }
