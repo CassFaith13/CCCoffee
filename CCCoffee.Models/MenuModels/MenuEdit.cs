4be990f38562829e5b016eaa5f4446f6b6783c61
Cassandra Faith <105570288+CassFaith13@users.noreply.github.com>
2023-01-18 18:46:25 -0500

    Update MenuEdit.cs

--- CCCoffee.Models/MenuModels/MenuEdit.cs
+++ CCCoffee.Models/MenuModels/MenuEdit.cs
@@ -1,17 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace CCCoffee.Models.Menu
+namespace CCCoffee.Models.MenuModels
 {
-    public class MenuCreate
+    public class MealEdit
     {
-        [Required]
-        [MaxLength(50, ErrorMessage = "Sorry you can only have 50 characters")]
         public string? MealName { get; set; }
-        [Required]
-        [MaxLength(150, ErrorMessage = "Meal Name cannot exceed 150 characters!")]
         public string? MealDescription { get; set; }
-        [Required]
         public decimal MealPrice { get; set; }
-        
     }
 }
