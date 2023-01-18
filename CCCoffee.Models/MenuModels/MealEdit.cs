ec32a02ae1d5e026635f780500764d2b81e3a936
Cassandra Faith <105570288+CassFaith13@users.noreply.github.com>
2023-01-17 19:27:48 -0500

    Update MealEdit.cs

--- CCCoffee.Models/MenuModels/MealEdit.cs
+++ CCCoffee.Models/MenuModels/MealEdit.cs
@@ -7,8 +7,8 @@ namespace CCCoffee.Models.MenuModels
 {
     public class MealEdit
     {
-        public string MealName { get; set;}
-        public string MealDescription { get; set;}
-        public string MealPrice { get; set;}
+        public string? MealName { get; set; }
+        public string? MealDescription { get; set; }
+        public decimal MealPrice { get; set; }
     }
 }
